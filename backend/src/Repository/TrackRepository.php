81e86bce0f1386f6707a63326e6e7fa9961a1b14
peeter <peeterjoe45@gmail.com>
2025-07-14 19:55:03 -0400

    feat: database & entity setup

---
+++ backend/src/Repository/TrackRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Track;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Track>
+ */
+class TrackRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Track::class);
+    }
+
+    public function save(Track $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Track $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+}
